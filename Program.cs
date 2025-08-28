23dccfb896a1f3212762221f3509902b1b7c7393
Denys <den.linevych@gmail.com>
2025-08-28 13:45:31 +0300

    Add auto-add functionality to wishlist

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.CommandLine;
-using System.ComponentModel.Design;
 
 namespace SteamWishlister;
 
@@ -44,6 +43,9 @@ class Program
         Command generateQueueCommand = new("genqueue", "Generate new queue of games");
         wishlistCommand.Subcommands.Add(generateQueueCommand);
 
+        Command autoAddCommand = new("autoadd", "Automatically adds games to your wishlist.");
+        wishlistCommand.Subcommands.Add(autoAddCommand);
+
         addCommand.SetAction(async parseresult =>
         {
             string? sessionId = parseresult.GetValue(sessionIdOption) ?? "";
@@ -54,11 +56,11 @@ class Program
             await wishlist.AddGameAsync(gameId);
         });
 
-        generateQueueCommand.SetAction(async parseresult =>
+        generateQueueCommand.SetAction(async parseResult =>
         {
             Console.WriteLine("Generating new game queue...");
-            string sessionId = parseresult.GetValue(sessionIdOption) ?? "";
-            string loginCookie = parseresult.GetValue(loginCookieOption) ?? "";
+            string sessionId = parseResult.GetValue(sessionIdOption) ?? "";
+            string loginCookie = parseResult.GetValue(loginCookieOption) ?? "";
 
             Wishlist wishlist = new(sessionId, loginCookie);
             string[] gameIds = await wishlist.GenerateNewQueue();
@@ -69,6 +71,36 @@ class Program
             foreach (string gameId in gameIds)
             {
                 Console.WriteLine(gameId);
+            }
+        });
+
+        autoAddCommand.SetAction(async parseResult =>
+        {
+            Console.WriteLine("Starting the process of automatically adding games to the wishlist.");
+            string sessionId = parseResult.GetValue(sessionIdOption) ?? "";
+            string loginCookie = parseResult.GetValue(loginCookieOption) ?? "";
+
+            Wishlist wishlist = new(sessionId, loginCookie);
+
+            while (true)
+            {
+                try
+                {
+                    var gameIds = await wishlist.GenerateNewQueue();
+                    if (!gameIds.Any())
+                    {
+                        Console.WriteLine("Game queue wan't retrieved.");
+                        return;
+                    }
+
+                    Console.WriteLine("Game queue retrieved.");
+                    await Task.WhenAll(gameIds.Select(wishlist.AddGameAsync));
+                    Thread.Sleep(5000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An error occurred: {e.Message}");
+                }
             }
         });
 
